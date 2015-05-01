05abb4d4669e860633cfde33f1c6a40409be192c
hal1437 <fripfrop@docomo.ne.jp>
2015-05-02 02:18:08 +0900

    [Added] Added SYNTHESIZE preprocesser. [Added] Added Contains class.

---
+++ Utility/Constants.hpp
@@ -0,0 +1,24 @@
+
+#pragma once
+
+class Contains{
+	enum class ZUKU{
+		EMPTY=0,
+		FILL =1,
+	};
+
+	enum class DIRECTION{
+		UP    = 0,
+		RIGHT = 0,
+		DOWN  = 0,
+		LEFT  = 0,
+	};
+	
+	enum class ANGLE{
+		ANGLE90  = 0,
+		ANGLE180 = 1,
+		ANGLE270 = 2,
+		ANGLE360 = 3,
+	}
+
+}
