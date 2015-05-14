20ce6413bce3c15bc4d135b07173a46456c1d668
hal1437 <fripfrop@docomo.ne.jp>
2015-05-14 17:38:14 +0900

    [Added] Added Answer class.

---
+++ Structure/Answer.cpp
@@ -0,0 +1,45 @@
+
+#include "Answer.h"
+#include <fstream>
+
+void Answer::SetField (Field fi){
+	this->field = fi;
+}
+void Answer::AddBlocks(){
+	hands.push_back(Hand{Block(),Point(-1,-1)});
+}
+void Answer::AddBlocks(Block block,Point pos,bool reverse,Constants::ANGLE angle){
+	hands.push_back(Hand{block,pos,angle,reverse});
+}
+bool Answer::Export(std::string filename)const{
+	std::ofstream ofs(filename);
+	if(!ofs)return false;
+	ofs << (this);
+	return true;
+}
+
+Field Answer::GetField(){
+	Field field = this->field;
+	for(Answer::Hand& hand : hands){
+		if(hand.pos != Point(-1,-1))field.Projection(hand.pos,hand.block);
+	}
+	return field;
+}
+
+std::ostream& operator<<(std::ostream& ost,const Answer& answer){
+	for(Answer::Hand hand : answer.hands){
+		if(hand.pos != Point(-1,-1)){
+			ost << hand.pos.x;
+			ost << " ";
+			ost << hand.pos.y;
+			ost << " ";
+			ost << (hand.reverse ? "T" : "H");
+			ost << " ";
+			ost << hand.angle;
+		}
+		ost << "\r\n";
+	}
+
+	return ost;
+}
+
--- Structure/test.cpp
+++ Structure/test.cpp
@@ -4,18 +4,26 @@
 #include <iostream>
 #include <fstream>
 #include "Problem.h"
+#include "Answer.h"
 
 int main(){
 
 	Problem prob("../Problem/Problem.txt");
+	Answer ans;
 
-	Field field = prob.GetField();
-	Block block = prob.GetBlock(0);
+	ans.SetField(prob.GetField());
+	ans.AddBlocks(prob.GetBlock(0),Point(1,1),false,Constants::ANGLE90);
 
-	std::cout << block << std::endl;
-	std::cout << "isLayPossible:" << field.isLayPossible(Point(3,0),block.GetRotate(Constants::ANGLE90)) << std::endl;
-	field.Projection(Point(3,0),block.GetRotate(Constants::ANGLE90));
-	std::cout << field << std::endl;
+	//Field field = prob.GetField();
+	//Block block = prob.GetBlock(0);
+
+	//std::cout << block << std::endl;
+	//std::cout << "isLayPossible:" << field.isLayPossible(Point(3,0),block.GetRotate(Constants::ANGLE90)) << std::endl;
+	//field.Projection(Point(3,0),block.GetRotate(Constants::ANGLE90));
+	std::cout << prob.GetField() << std::endl;
+	std::cout << ans.GetField()  << std::endl;
+	std::cout << ans             << std::endl;
+
 
 	return 0;
 }
