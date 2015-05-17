a7dfbec6629c11201bbeec031e9749cd95f48aec
hal1437 <fripfrop@docomo.ne.jp>
2015-05-17 16:35:51 +0900

    [Changed] Change to use vairiable length constructor.

--- Structure/Heuristic.h
+++ Structure/Heuristic.h
@@ -1,9 +1,13 @@
 
 #pragma once
 
-template<class ReturnType,class ArgType>
+template<class ReturnType,class... ArgType>
 class Heuristics{
 public:
-	virtual ReturnType Execution(const ArgType& arg)=0;
+	virtual ReturnType Execution(const ArgType&... arg)=0;
+
+	ReturnType operator()(const ArgType&... arg){
+		return Execution(arg...);
+	}
 };
 
--- Structure/test.cpp
+++ Structure/test.cpp
@@ -6,12 +6,24 @@
 #include "Problem.h"
 #include "Answer.h"
 #include "Solver.h"
+#include "Heuristic.h"
+
+class Addition:public Heuristics<int,int,int>{
+public:
+	int Execution(const int& a,const int& b){
+		return a+b;
+	}
+	
+};
+
 
 int main(){
 
 	Problem prob("../Problem/Problem.txt");
 	Answer ans;
 	Solver* solv;
+	Heuristics<int,int,int>* hs;
+	hs = new Addition();
 
 	ans.SetField(prob.GetField());
 	ans.AddBlocks(prob.GetBlock(0),Point(1,1),false,Constants::ANGLE90);
@@ -19,6 +31,7 @@ int main(){
 	std::cout << prob.GetField() << std::endl;
 	std::cout << ans.GetField()  << std::endl;
 	std::cout << ans             << std::endl;
+	std::cout << hs->Execution(10,10) << std::endl;
 
 	return 0;
 }
