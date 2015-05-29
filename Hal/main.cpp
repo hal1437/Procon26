9d5743597f039a0eddf2be15383a38d8f550dbff
Hal Stroemeria <fripfrop@docomo.ne.jp>
2015-05-29 19:13:05 +0900

    [Added] Added WeightComposit class

--- Hal/main.cpp
+++ Hal/main.cpp
@@ -4,19 +4,23 @@
 #include "../Structure/Problem.h"
 #include "Heuristics/DensityAround.h"
 #include "Heuristics/Cavity.h"
+#include "Heuristics/WeightComposit.h"
 #include "Solver/AStar.h"
 #include "Solver/BestFirst.h"
 
 int main(){
 	std::cout << "----Begin of program----" << std::endl;
-	Problem prob("../Problem/quest2.txt");
+	Problem prob("../Problem/quest1.txt");
 	std::ofstream ofs("Answer.txt");
 	Solver* solver;
-	Heuristics<unsigned,Field>* density;
+	WeightComposit* density;
 
-	density = new DensityAround();
+	density = new WeightComposit();
 	solver  = new BestFirst(prob,density);
 	
+	density->AddHeuristic(new DensityAround(),1.0f);
+	density->AddHeuristic(new Cavity(),8.0f);
+
 	ofs << solver->Solve() << std::endl;
 
 	std::cout << "-----End of program-----" << std::endl;
