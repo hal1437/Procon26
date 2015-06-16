2f8818c2369f15247b6c610e64926f9d6b1c3c39
Hal Stroemeria <fripfrop@docomo.ne.jp>
2015-06-16 19:34:59 +0900

    [BugFix]WeightCompositの戻り値をunsigendからdoubleに変更

--- Hal/Heuristics/WeightComposit.cpp
+++ Hal/Heuristics/WeightComposit.cpp
@@ -1,13 +1,13 @@
 
 #include "WeightComposit.h"
 
-void WeightComposit::AddHeuristic(h_type* heuristics,float weight){
-	h_list.insert(std::pair<h_type*,float>(heuristics,weight));
+void WeightComposit::AddHeuristic(h_type* heuristics,double weight){
+	h_list.insert(std::pair<h_type*,double>(heuristics,weight));
 }
 
-unsigned WeightComposit::Execution(const Field& field){
-	unsigned score = 0;
-	for(std::pair<h_type*,float> h : h_list){
+double WeightComposit::Execution(const Field& field){
+	double score = 0;
+	for(std::pair<h_type*,double> h : h_list){
 		score += h.first->Execution(field) * h.second;
 	}
 	return score;
