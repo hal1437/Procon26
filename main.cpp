f52430453901f1a8e78a9197dd4fa3a6f9b1e05a
Hal Stroemeria <fripfrop@docomo.ne.jp>
2015-06-24 01:12:39 +0900

    [BugFix]Fixed bug in Normalize() if block size isn't BLOCK_SIZE and FIELD_SIZE

--- main.cpp
+++ main.cpp
@@ -15,16 +15,25 @@ int main(){
 	Field field;// = prob.GetField();
 	Answer ans(prob);
 	
+	Matrix<3,3> mat;
+	mat[1][0]=1;
+	mat[1][1]=1;
+	mat[0][0]=1;
+	mat[2][2]=1;
+	std::cout << mat << std::endl;
+	std::cout << mat.GetNormalize() << std::endl;
+
 	//===========BENCHMARK RESULT===========
 	//    [COUNT]          500 times
 	//[FULL TIME]         9756 msec
 	// [PER TIME]       19.512 msec/function 
 	//======================================
-	
+	/*
 	std::cout << prob.GetField() << std::endl;
 	BenchMark<500>()([&](){
 		prob.GetField().GetListLayPossible(prob.GetBlock(0));
 	});
+	*/
 	//std::cout << prob.GetField() << std::endl;
 	
 	return 0;
