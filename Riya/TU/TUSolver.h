25ce176d74289fe8d5920889ff4c93c00ac34d90
Riyaaaaa <s113111@edu.asahikawa-nct.ac.jp>
2015-06-24 18:45:26 +0900

    [added] TUSolver

--- Riya/TU/TUSolver.h
+++ Riya/TU/TUSolver.h
@@ -9,5 +9,19 @@
 #ifndef Procon26_project_TUSolver_h
 #define Procon26_project_TUSolver_h
 
+#include<vector>
 
+template<class _Tp,class _ChangeState>
+class TUSolver{
+    const int NEIGHTBORHOOD,TABU_LIST_SIZE;
+    using state_type = _Tp::_State;
+    using aux_type = _Tp::_Aux;
+    
+    std::vector<_ChangeState> _tabu_list;
+    
+public:
+    TUSolver(const int NEIGHTBORHOOD,LIST_SIZE): NEIGHTBORHOOD(NEIGHTBORHOOD),LIST_SIZE(LIST_SIZE){}
+    TUSolver(): NEIGHTBORHOOD(5),LIST_SIZE(100){}
+    state_type solveAnswer();
+};
 #endif
