5c83e41cccba5e045193ad5e09c2ad17893cf79e
Hal Stroemeria <fripfrop@docomo.ne.jp>
2015-07-23 19:09:14 +0900

    [Added]Added Transform::operator<

--- Structure/Transform.cpp
+++ Structure/Transform.cpp
@@ -1,6 +1,14 @@
 
 #include "Transform.h"
 
+bool Transform::operator<(const Transform& rhs)const{
+	if     (this->pos.x   != rhs.pos.x  )return this->pos.x < rhs.pos.x;
+	if     (this->pos.y   != rhs.pos.y  )return this->pos.y < rhs.pos.y;
+	else if(this->angle   != rhs.angle  )return this->angle < rhs.angle;
+	else if(this->reverse != rhs.reverse)return this->reverse < rhs.reverse;
+	else false;
+}
+
 std::ostream& operator<<(std::ostream& ost,const Transform& trans){
 	if(trans.isEnable() == false) ost << "disable_transform";
 	ost << std::boolalpha;
